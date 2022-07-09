cf3ad167f476fcfa4991679fa3047cba6bd32ea9
dazhi <ch2ohch2oh@gmail.com>
2022-07-09 11:21:17 -0700

    Add traverser

--- compiler.js
+++ compiler.js
@@ -117,4 +117,37 @@ function parser(tokens) {
   return ast;
 }
 
+function traverser(ast, visitor) {
+  function traverseArray(array, parent) {
+    array.forEach((child) => {
+      trverseNode(child, parent);
+    });
+  }
+
+  function traverseNode(node, parent) {
+    let methods = visitor[node.type];
+    if (methods && methods.enter) {
+      methods.end(node, parent);
+    }
+    switch (node.type) {
+      case "Program":
+        traverseArray(node.body, node);
+        break;
+      case "CallExpression":
+        traverseArray(node.params, node);
+        break;
+      case "NumberLiteral":
+      case "StringLiteral":
+        break;
+      default:
+        throw new TypeError("Invalid node: " + JSON.stringify(node));
+        break;
+    }
+    if (methods && methods.exit) {
+      methods.exit(node, parent);
+    }
+  }
+
+  traverseNode(ast, null);
+}
 export { tokenizer, parser };
